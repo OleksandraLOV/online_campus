e44753e517ed293d92c6da2a36384f33a89ac1dc
DiperCool <githubdiper@gmail.com>
2026-04-17 22:29:09 +0300

    rm node_modules & dist

--- server/dist/references/references.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { ReferencesService } from './references.service';
-export declare class ReferencesController {
-    private referencesService;
-    constructor(referencesService: ReferencesService);
-    getGroups(): import("../common/types/entities").Group[];
-    getClassrooms(): import("../common/types/entities").Classroom[];
-    getDepartments(): import("../common/types/entities").Department[];
-    getFaculties(): import("../common/types/entities").Faculty[];
-}
--- server/dist/references/references.service.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class ReferencesService {
-    getGroups(): import("../common/types/entities").Group[];
-    getClassrooms(): import("../common/types/entities").Classroom[];
-    getDepartments(): import("../common/types/entities").Department[];
-    getFaculties(): import("../common/types/entities").Faculty[];
-}
